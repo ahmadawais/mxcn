4a41c5fd06bb0bf7b83a3e36468eca09c64a929d
Ahmad Awais <me@AhmadAwais.com>
2023-06-18 14:23:38 +0500

    🐛 FIX: Sync .gitignore

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { ClassValue } from 'clsx';
-
-declare function cn(...inputs: ClassValue[]): string;
-
-export { cn, cn as default };
--- dist/index.js
+++
@@ -1,2 +0,0 @@
-import{clsx as e}from"clsx";import{twMerge as t}from"tailwind-merge";function o(...r){return t(e(r))}var m=o;export{o as cn,m as default};
-//# sourceMappingURL=index.js.map
